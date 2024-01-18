e1456ceed5714976760b2d236457330b9395e792
lazar-cvetkovic <laki.cvetkovic@gmail.com>
2024-01-18 18:54:08 +0100

    Common project setup

---
+++ Common/Communication/Sender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Communication
+{
+    public class Sender
+    {
+        private NetworkStream _stream;
+        private BinaryFormatter _formatter;
+        private Socket _socket;
+
+        public Sender(Socket socket)
+        {
+            _socket = socket;
+            _stream = new NetworkStream(socket);
+            _formatter = new BinaryFormatter();
+        }
+
+        public void Send(object argument)
+        {
+            _formatter.Serialize(_stream, argument);
+        }
+
+    }
+}
---
+++ Common/Helpers/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Helpers
+{
+    public static class StringExtensions
+    {
+        public static bool IsAllLetters(this string str)
+            => (str != null) && str.All(c => char.IsLetter(c));
+
+        public static bool IsAllNumbers(this string str)
+            => (str != null) && str.All(c => char.IsDigit(c));
+
+        public static bool AreNullOrEmpty(params string[] strings)
+            => (strings == null) || strings.Any(s => string.IsNullOrEmpty(s));
+    }
+}
