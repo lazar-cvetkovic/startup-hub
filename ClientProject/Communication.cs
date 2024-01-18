4ce6fcb33efc8da7754fc4f53a2527c2b7c765f5
lazar-cvetkovic <laki.cvetkovic@gmail.com>
2024-01-18 19:18:39 +0100

    Server project setup

---
+++ ClientProject/Communication.cs
@@ -0,0 +1,39 @@
+﻿using Common.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientProject
+{
+    internal class Communication
+    {
+        private static Communication _instance;
+        public static Communication Instance
+        {
+            get
+            {
+                _instance = _instance ?? new Communication();
+                return _instance;
+            }
+        }
+
+        private Socket _socket;
+        private Sender _sender;
+        private Receiver _receiver;
+
+        private Communication() { }
+
+        public void Connect()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Connect("127.0.0.1", 9999);
+
+            _sender = new Sender(_socket);
+            _receiver = new Receiver(_socket);
+        }
+
+    }
+}
---
+++ ClientProject/GUIControllers/MainCoordinator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientProject.GUIControllers
+{
+    internal class MainCoordinator
+    {
+        private static MainCoordinator _instance;
+        public static MainCoordinator Instance
+        {
+            get
+            {
+                _instance = _instance ?? new MainCoordinator();
+                return _instance;
+            }
+        }
+
+        private MainCoordinator() { }
+    }
+}
