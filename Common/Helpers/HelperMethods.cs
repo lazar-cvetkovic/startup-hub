6fd2194d9820bef254322ccceb3959c476840d7e
lazar-cvetkovic <laki.cvetkovic@gmail.com>
2024-01-20 09:54:05 +0100

    Added message box helper methods

---
+++ Common/Helpers/HelperMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Common.Helpers
+{
+    public static class HelperMethods
+    {
+        public static void ShowInfoMessage(string message) => MessageBox.Show(message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        public static void ShowErrorMessage(string message) => MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        public static void ShowWarningMessage(string message) => MessageBox.Show(message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        public static DialogResult ShowYesNoMessage(string message) => MessageBox.Show(message, "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+    }
+}
